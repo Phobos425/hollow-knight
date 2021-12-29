4a23b368cd3033bc3084416e568a7ef79f741477
Phobos425 <asuhomlin2007@yandex.ru>
2021-12-29 16:17:49 +0300

    test

--- data/platform.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="platform" tilewidth="32" tileheight="32" tilecount="504" columns="28">
- <image source="kisspng-pixel-art-tile-based-video-game-2d-platform-png-5b12975be8efa5.1609509215279450519541.jpg" width="900" height="600"/>
-</tileset>
--- data/winter.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="winter" tilewidth="32" tileheight="32" tilecount="374" columns="22">
- <image source="winter.png" width="725" height="567"/>
-</tileset>
