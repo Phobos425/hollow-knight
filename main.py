a830b8b6815acb081c6b535c05e30d98d9a296d6
Phobos425 <94921371+Phobos425@users.noreply.github.com>
2022-02-20 16:20:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+import game
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+g = game.game()
+# Press the green button in the gutter to run the script.
+try:
+    if __name__ == '__main__':
+        g.gameloop()
+except:
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
