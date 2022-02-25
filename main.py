faabcb7d79a796668d704adfca67b44b90d44e9d
Phobos425 <94921371+Phobos425@users.noreply.github.com>
2022-02-25 19:01:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# This is a sample Python script.
 import game
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sqlite3
 
 
 g = game.game()
-# Press the green button in the gutter to run the script.
-try:
-    if __name__ == '__main__':
-        g.gameloop()
-except:
-    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+        g.gameloop()
+        con = sqlite3.connect("result.db")
+
+        cur = con.cursor()
+        cur.execute(f"""INSERT INTO result VALUES('{game.delta_time ,game.platformer.coins}')""")
+
